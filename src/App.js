bcab90d3462207641e7e630145441320741179e5
HimanshuHeda <harsh967896@gmail.com>
2024-07-16 00:09:44 +0530

    files

--- src/App.js
+++ src/App.js
@@ -24,11 +24,13 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = 'black';
       showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
     }
   }
 
